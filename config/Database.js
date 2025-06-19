e1d50323882707485f8ceba2031b2339bd716324
itneyin <164400866+itneyin@users.noreply.github.com>
2025-06-19 23:09:27 +0200

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,19 +1,26 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 class Database {
-	constructor(){ this.connection = null }
-	connect(){
-		console.log('Connecting to database...')
-		mongoose.set('strictQuery', true)
-		mongoose.connect(process.env.MONGO_URL, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		}).then(() => {
-			console.log('Connected to database')
-			this.connection = mongoose.connection
-		}).catch(err => {
-			console.error(err)
-		})
-	}
+  constructor() {
+    this.connection = null;
+  }
+
+  connect() {
+    console.log('Connecting to database...');
+    mongoose.set('strictQuery', true);
+
+    // Return the promise so you can await or chain it
+    return mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then(() => {
+      console.log('Connected to database');
+      this.connection = mongoose.connection;
+    }).catch(err => {
+      console.error('Database connection error:', err);
+    });
+  }
 }
-module.exports = Database
+
+module.exports = Database;
